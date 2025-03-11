4f8277c7a9bdbc7df3ce73e1452e518ab2de7481
RyosukeMatsushima <r.matsushima@space-walker.co.jp>
2025-03-11 16:46:29 +0900

    change git default editor as nvim

--- setup_git.sh
+++ setup_git.sh
@@ -7,3 +7,12 @@ git config --global user.email "$email"
 # Prompt for the user name associated with GitHub account
 read -p "Enter your GitHub username: " username
 git config --global user.name "$username"
+
+
+# Set nvim as the default editor for Git
+git config --global core.editor "nvim"
+
+# Confirm the change
+echo "Git default editor set to nvim:"
+git config --global core.editor
+
